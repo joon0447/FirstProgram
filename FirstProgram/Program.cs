7fc6b0064ea0daf58b770ea00005580f585ef987
joon0447 <joon0447@naver.com>
2024-03-04 13:25:13 +0900

    초기 커밋.

---
+++ FirstProgram/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace FirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Write");
+            Console.WriteLine("Write Line");
+
+            Console.WriteLine("Write Line");
+            Console.WriteLine("Write Line");
+
+            Console.Write("Write");
+            Console.Write("Write");
+
+        }
+    }
+}
